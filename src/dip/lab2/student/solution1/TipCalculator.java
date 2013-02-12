54bc5682ce43cbdaa5fe9e52dc12f58f24d15b77
SCaddell@my.wctc.edu <>
2013-02-11 18:03:18 -0600

    First commit for Lab 2

---
+++ src/dip/lab2/student/solution1/TipCalculator.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab2.student.solution1;
+
+/**
+ *
+ * @author Stuart Caddell
+ */
+public interface TipCalculator {
+    
+    /**
+     * Sets the numberr of units the tip is to be based upon.
+     * For food service it would be bill amount
+     * For baggage, it would be the number of bags
+     * Each Tip type should have it's own "based upon"
+     */
+    void setNumUnitsTipBasedUpon(double numOfUnits);
+    
+    /**
+     * Sets the Service Rating, or the quality of service received.
+     */
+    void setServiceRating(String serviceQuality);
+    
+    /**
+     * Gets the tip amount 
+     */
+    double getTip();
+    
+    
+    
+}
